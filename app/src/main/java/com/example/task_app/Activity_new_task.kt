12e27600fb920df2a3f89b5d7c4a6956f7cb76f8
JoseLeandroNascimento <j.leandro1998silva@gmail.com>
2025-02-04 10:01:40 -0500

    feat: layout new task

--- app/src/main/java/com/example/task_app/Activity_new_task.kt
+++ app/src/main/java/com/example/task_app/Activity_new_task.kt
@@ -1,6 +1,5 @@
 package com.example.task_app
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.activity.enableEdgeToEdge
@@ -8,27 +7,26 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class Activity_new_task : AppCompatActivity() {
 
-    private lateinit var btnNewTask:Button
-    private lateinit var btnMyTasks:Button
+    private lateinit var btnSave: Button
+    private lateinit var btnBack: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_new_task)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
 
-        btnNewTask = findViewById(R.id.btn_new_task)
-        btnMyTasks = findViewById(R.id.btn_my_tasks)
+        btnSave = findViewById(R.id.btn_save)
+        btnBack = findViewById(R.id.btn_back)
 
-        btnNewTask.setOnClickListener {
-            val intent = Intent(this,Activity_new_task::class.java)
-            startActivity(intent)
+        btnBack.setOnClickListener {
+            finish()
         }
 
     }
